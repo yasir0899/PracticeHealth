2d2b550f59f4e20e2a1c607083fb034d4f70f0d9
Muhammad Yasir Khan <yasir0899@gmail.com>
2019-07-30 12:52:00 +0500

    locally commit

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -17,7 +17,7 @@ class MainActivity : AppCompatActivity(),naigationToFragment {
         setSupportActionBar(toolbar)
         naigateToListView(null)
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+            Snackbar.make(view, "Replace with your own action ", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
     }
